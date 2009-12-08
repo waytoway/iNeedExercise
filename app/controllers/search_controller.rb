d4cdaac03db82bad6eae019bf804802bf37020b8
project1 <suntianwei@live.com>
2009-12-08 21:36:39 +0800

    add some controllers and views

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,14 @@
+class SearchController < ApplicationController
+  # Be sure to include AuthenticationSystem in Application Controller instead
+  include AuthenticatedSystem
+  # If you want "remember me" functionality, add this before_filter to Application Controller
+  before_filter :login_from_cookie
+
+  def index
+    
+  end
+  
+  def detail
+    
+  end
+end
