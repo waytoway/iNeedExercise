01ea5c23377b44140d8a8785152a0d560f45cca5
unknown <andy@.(none)>
2009-12-09 17:07:41 +0800

    userLogin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,16 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  
+  
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
   def index
-    
+    puts 'hello1'
   end
+  
+  
 end
