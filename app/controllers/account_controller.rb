ea17aa9c0e16b0263c061ad1004242581adf668f
project1 <suntianwei@live.com>
2009-12-09 22:08:16 +0800

    登录后显示姓名

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,26 +3,27 @@ class AccountController < ApplicationController
   include AuthenticatedSystem
   # If you want "remember me" functionality, add this before_filter to Application Controller
   before_filter :login_from_cookie
-
   # say something nice, you goof!  something sweet.
   def index
     #redirect_to(:action => 'signup') unless logged_in? || User.count > 0
     
   end
-
+  
+  
   def login
     return unless request.post?
     self.current_user = User.authenticate(params[:login], params[:password])
+    $useraccount=params[:login]
     if logged_in?
       if params[:remember_me] == "1"
         self.current_user.remember_me
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
       end
-      redirect_back_or_default(:controller => '/account', :action => 'index')
+      redirect_back_or_default(:controller => 'main', :action => 'index')
       flash[:notice] = "Logged in successfully"
     end
   end
-
+  
   def signup
     @user = User.new(params[:user])
     return unless request.post?
