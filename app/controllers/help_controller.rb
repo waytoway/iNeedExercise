62d0ac67648dad2f6cdad033c13f21b43b28abd1
project1 <suntianwei@live.com>
2009-12-11 06:41:35 +0800

    添加我的锻炼视图，修改model，将user,t_member_card,t_card_usage_record生成关系

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -5,6 +5,12 @@ class HelpController < ApplicationController
   before_filter :login_from_cookie
 
   def index
-    
+#    a=User.find(1)
+#    puts "aaaaaaaaaaaaaaaa"
+#    puts a.t_member_cards[0][:ID]
+#    puts a.t_member_cards[0].t_card_usage_records.length
+#    a=TMemberCard.find(1)
+#    puts "aaaaaaaaaaaaaaaa"
+#    puts a.t_card_usage_records[0][:card_no]
   end
 end
