79f59507500381c2943c9c64d09ba5cb2093d8f0
unknown <andy@.(none)>
2009-12-11 02:35:43 +0800

    User Controller index

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,9 @@
 class UserController < ApplicationController
   before_filter :login_required
-  
+  include AuthenticatedSystem
+  before_filter :login_from_cookie
+  def index
+    #redirect_to(:action => 'signup') unless logged_in? || User.count > 0
+    
+  end
 end
