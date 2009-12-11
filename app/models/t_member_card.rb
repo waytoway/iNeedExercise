b7d43311bdd86ff8fd3adf99d9290d1d164f8478
project1 <suntianwei@live.com>
2009-12-11 17:25:26 +0800

    建立user和card，order关系

--- app/models/t_member_card.rb
+++ app/models/t_member_card.rb
@@ -1,5 +1,4 @@
 class TMemberCard < ActiveRecord::Base
   set_table_name "t_member_card"
-  belongs_to :user,:class_name=>"User",:foreign_key=>"NAME",:primary_key=>"login"
-  has_many :t_card_usage_records,:class_name=>"TCardUsageRecord",:foreign_key=>"card_id",:primary_key=>"ID"
+  has_one :users_card,:class_name=>"UsersCard",:foreign_key=>"card_id",:primary_key=>"ID"
 end
