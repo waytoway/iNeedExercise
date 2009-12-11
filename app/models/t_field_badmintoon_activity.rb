c06740ea3734aecba62b9eb85b296312d8d9a4f6
unknown <andy@.(none)>
2009-12-11 22:29:26 +0800

    create relation between venue and fields

--- app/models/t_field_badmintoon_activity.rb
+++ app/models/t_field_badmintoon_activity.rb
@@ -1,3 +1,4 @@
 class TFieldBadmintoonActivity < ActiveRecord::Base
   set_table_name "t_field_badmintoon_activity"
+  belongs_to :t_venue_info,:class_name=>"TVenueInfo",:foreign_key=>"VENUE_ID",:primary_key=>"ID"
 end
