ba696db5d0328a73e2762803e98660a63044a102
unknown <andy@.(none)>
2009-12-18 07:21:23 +0800

    add order list

--- app/models/t_field_order.rb
+++ app/models/t_field_order.rb
@@ -1,4 +1,5 @@
 class TFieldOrder < ActiveRecord::Base
   set_table_name "t_field_order"
   has_one :user_order,:class_name=>"UsersOrder",:foreign_key=>"order_id",:primary_key=>"ID"
+  belongs_to :t_venue_info,:class_name=>"TVenueInfo",:foreign_key=>"VENUE_ID",:primary_key=>"ID"
 end
--- app/models/t_member_card.rb
+++ app/models/t_member_card.rb
@@ -1,4 +1,4 @@
 class TMemberCard < ActiveRecord::Base
   set_table_name "t_member_card"
-  has_one :users_card,:class_name=>"UsersCard",:foreign_key=>"ID",:primary_key=>"card_id"
+  has_one :users_card,:class_name=>"UsersCard",:foreign_key=>"card_id",:primary_key=>"ID"
 end
