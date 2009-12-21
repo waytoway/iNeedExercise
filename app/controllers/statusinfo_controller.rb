ec73f57275ac35e3faa816f36f3b8395e986f696
project1 <suntianwei@live.com>
2009-12-21 11:06:37 +0800

    状态信息页面基本完成

--- app/controllers/statusinfo_controller.rb
+++ app/controllers/statusinfo_controller.rb
@@ -6,37 +6,44 @@ class StatusinfoController < ApplicationController
   
   
   def index
-    #测试数据====
-    params[:venue_id]=2
-    params[:from_time]="07:00"
-    params[:field_type]="羽毛球"
-    params[:usable_time]="2009-12-20"
-    #测试数据====
-    @tab=[]
     #如果传进来的是查询某项目状态，则返回一个tab
     if params[:venue_id]!=nil && params[:from_time]!=nil 
+      @tabs=[]
       @span_time=Time.parse params[:from_time] 
       @span_time=@span_time+3600
       if params[:field_type]!=nil
         #查询所有的符合日期，时间，场馆，项目的记录
         @query_records =TFieldBadmintoonActivity.find_by_sql("select * from t_field_badmintoon_activity where VENUE_ID='#{params[:venue_id]}' and FIELD_TYPE='#{params[:field_type]}' and FROM_TIME>='#{params[:from_time]}' and FROM_TIME<'#{@span_time.strftime("%H:%M").to_s}' and USABLE_DATE='#{params[:usable_time]}'")
-        @tab.push(params[:field_type])  
-      #如果传进来的是查询场馆状态
+        @tabs.push(params[:field_type])  
       else
-        @query_records=TFieldBadmintoonActivity.find_by_sql("select * from t_field_badmintoon_activity where VENUE_ID='#{params[:venue_id]}' and FROM_TIME>='#{params[:from_time]}' and FROM_TIME<'#{@span_time.strftime("%H:%M").to_s}' and USABLE_DATE='#{params[:usable_time]}'")
+      #如果传进来的是查询场馆状态
+        @records=TFieldBadmintoonActivity.find_by_sql("select * from t_field_badmintoon_activity where VENUE_ID='#{params[:venue_id]}' and FROM_TIME>='#{params[:from_time]}' and FROM_TIME<'#{@span_time.strftime("%H:%M").to_s}' and USABLE_DATE='#{params[:usable_time]}'")
+        if @records!=nil
+          #查询所有设施种类
+          @records.each do |record|
+            if @tabs.include?(record[:FIELD_TYPE])==false
+               @tabs.push(record[:FIELD_TYPE])
+            end
+          end
+          #查询第一个项目记录
+          @query_records= TFieldBadmintoonActivity.find_by_sql("select * from t_field_badmintoon_activity where VENUE_ID='#{params[:venue_id]}' and FIELD_TYPE='#{@tabs[0]}' and FROM_TIME>='#{params[:from_time]}' and FROM_TIME<'#{@span_time.strftime("%H:%M").to_s}' and USABLE_DATE='#{params[:usable_time]}'")
+        end
       end
-    else
-      redirect_to "http://bbs.nju.edu.cn"
-    end
-  end
-  
-  def show_map
-    redirect_to params[:url]
-  end
-  
-  def get_field
-    render :update do |page|
-      page.replace_html 'content' , :partial => 'status'
-    end
-  end
+  else
+  redirect_to "http://bbs.nju.edu.cn"
+end
+end
+
+def show_map
+redirect_to params[:url]
+end
+
+def get_field
+      @span_time=Time.parse params[:from_time] 
+      @span_time=@span_time+3600
+      @query_records =TFieldBadmintoonActivity.find_by_sql("select * from t_field_badmintoon_activity where VENUE_ID='#{params[:venue_id]}' and FIELD_TYPE='#{params[:field_type]}' and FROM_TIME>='#{params[:from_time]}' and FROM_TIME<'#{@span_time.strftime("%H:%M").to_s}' and USABLE_DATE='#{params[:usable_time]}'")
+      render :update do |page|
+page.replace_html 'content' , :partial => 'status'
+end
+end
 end
